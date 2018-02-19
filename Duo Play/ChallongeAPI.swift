5c3b67c0b82bca657811f163d6c22db11c6272ba
Jordan Davis <jdavisdev@gmail.com>
2018-02-19 14:19:44 -0700

    tournament branch creation hook up to challenge

---
+++ Duo Play/ChallongeAPI.swift
@@ -0,0 +1,14 @@
+//
+//  ChallongeAPI.swift
+//  Duo Play
+//
+//  Created by Jordan Davis on 2/19/18.
+//  Copyright © 2018 Jordan Davis. All rights reserved.
+//
+
+import Foundation
+
+public class ChallongeAPI {
+    static let challongeBaseUrl = "https://api.challonge.com/v1/"
+    static let API_KEY = "dtxaTM8gb4BRN13yLxwlbFmaYcteFxWwLrmAJV3h"
+}
